8fc5e3c959f59f332bc68fb20c948c952e8e2861
OviMori <vimus46@gmail.com>
2021-02-16 22:58:35 +0100

    Corretto Bug Scritta sovrapposta nel cambio di fragment.
    Il bag e' stato risolto implementando uno sfondo bianco per ogni fragment.

--- app/src/main/java/com/example/mytzilleri/MainActivity.java
+++ app/src/main/java/com/example/mytzilleri/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.mytzilleri;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottNavMenu = (BottomNavigationView) findViewById(R.id.bottNavMenu);
+        bottNavMenu.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new ProfiloFrag()).commit();
+
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item){
+            Fragment fragment = null;
+
+            switch(item.getItemId()){
+                case R.id.turniFrag:
+                    fragment = new TurniFrag();
+                    break;
+
+                case R.id.profiloFrag:
+                    fragment = new ProfiloFrag();
+                    break;
+
+                case R.id.chatFrag:
+                    fragment = new ChatFrag();
+                    break;
+
+                case R.id.magazzinoFrag:
+                    fragment = new MagazzinoFrag();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+
+            return true;
+        }
+    };
 }
