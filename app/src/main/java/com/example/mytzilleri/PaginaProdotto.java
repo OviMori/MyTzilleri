72cfa6bc1b8a4d7e9df793b3f311de77b0905131
OviMori <vimus46@gmail.com>
2021-03-10 21:16:18 +0100

    Fragment MagazzinoFrag
    Info Fornitore aggiunte
    Ottimizzata leggibilita' del codice con la creazione di stili
    
    Da implementare:
    Fare in modo che l immagine del prodotto scrolli insieme al resto degli elementi

--- app/src/main/java/com/example/mytzilleri/PaginaProdotto.java
+++ app/src/main/java/com/example/mytzilleri/PaginaProdotto.java
@@ -1,23 +1,40 @@
 package com.example.mytzilleri;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.AppComponentFactory;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class PaginaProdotto extends AppCompatActivity {
 
+    private static final int PICK_FROM_GALLERY = 1;
     SwitchCompat mySwitch;
-    ImageView imagineProdotto;
+    ImageView immagineProdotto, back_button;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,14 +42,39 @@ public class PaginaProdotto extends AppCompatActivity {
         setContentView(R.layout.pagina_prodotto);
 
         mySwitch = findViewById(R.id.switch_notifica_esaurimento);
-        imagineProdotto = findViewById(R.id.image_prodotto);
+        immagineProdotto = findViewById(R.id.immagine_prodotto);
+        back_button = findViewById(R.id.back_button);
 
-        imagineProdotto.setOnClickListener(new View.OnClickListener() {
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    PaginaProdotto.super.onBackPressed();
+            }
+        });
+
+        immagineProdotto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //procedura per cambiare immagine del prodotto
+
+                try {
+                    if (ActivityCompat.checkSelfPermission(PaginaProdotto.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(PaginaProdotto.this, new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
+                    } else {
+
+                        selectImage(PaginaProdotto.this);
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
         });
+
+
+
+
 
 
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -46,4 +88,69 @@ public class PaginaProdotto extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        immagineProdotto.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                immagineProdotto.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                //cursor.close();
+                            }
+                        }
+
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your profile picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+
 }
